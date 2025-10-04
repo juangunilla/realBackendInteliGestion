d2b978653ad7728154fd50a232d7700ebbfa310a
Juan <gunillajuan@gmail.com>
2025-10-04 19:33:36 -0300

    remove cors

--- index.js
+++ index.js
@@ -73,13 +73,7 @@ app.use(express.json());
 
 console.log(health);
 
-// Configurar cabeceras CORS
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+
 
 
 
